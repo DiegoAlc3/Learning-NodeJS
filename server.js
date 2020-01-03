aeb38d9792d96c8fd41a4d7521ff7287e2b3351c
Diego Alcantara <diegosalc15@gmail.com>
2020-01-03 15:56:45 -0300

    First model and connecting to database

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+//App
 const app = express();
 
-app.listen(3001);
+//Database
+mongoose.connect('mongodb://localhost:27017/nodeapi' ,  
+  {useNewUrlParser:true},
+);
+requireDir('./src/models');
+const Product = mongoose.model('Product');
 
+
+//First Route
 app.get('/', (req,res)=>{
-  res.send('Hello world');
+  Product.create({
+    title:'Manga',
+    description:'Fruta',
+    url:'https://www.google.com'
+  })
+  return res.send('Hello world');
 })
+
+app.listen(3001);
