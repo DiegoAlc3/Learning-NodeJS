ce7bfc319a7e0e4cafd8e84be79de9e6a138947f
Diego Alcantara <diegosalc15@gmail.com>
2020-01-03 17:01:01 -0300

    Finish API REST

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 //App
 const app = express();
-
+//Permitindo dados JSON
+app.use(express.json());
+//Permitindo dominios
+app.use(cors());
 //Database
 mongoose.connect('mongodb://localhost:27017/nodeapi' ,  
   {useNewUrlParser:true},
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,8 +3,32 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product')
 
 module.exports = {
+  //LIST
   async index(req,res){
-    const products = await Product.find();
+    const { page = 1 } = req.query;
+    const products = await Product.paginate({},{ page, limit:10});
     return res.json(products);
   },
+  //FIND ONE
+  async show(req,res){
+    const product = await Product.findById(req.params.id);
+
+    return res.json(product)
+  },
+  //ADD
+  async store(req,res){
+    const product = await Product.create(req.body);
+
+    return res.json(product);
+  },
+  //UPDATE
+  async update(req,res){
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body,{new:true});
+    return res.json(product);
+  },
+  async destroy(req,res){
+    await Product.findByIdAndRemove(req.params.id);
+    
+    return res.send('Produto deletado com sucesso')
+  }
 };
